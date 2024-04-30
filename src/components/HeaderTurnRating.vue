c49dabd741393f57a8af78994691014e07803af2
amel <mohaddese.computer@gmail.com>
2024-04-30 10:40:07 +0330

    chnage name next Icon in header

--- src/components/HeaderTurnRating.vue
+++ src/components/HeaderTurnRating.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="Ratings bg-white p-5 flex justify-between items-center">
         <button class="prev flex text-sm">
-          ماه بعد
           <svg
             class="h-5 w-5 text-red-500"
             width="24"
@@ -16,6 +15,7 @@
             <path stroke="none" d="M0 0h24v24H0z" />
             <polyline points="15 6 9 12 15 18" />
           </svg>
+          ماه بعد
         </button>
         <div class="title">
           <h1 class="font-bold text-lg">اردیبهشت 1403</h1>
