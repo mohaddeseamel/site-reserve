c95eb4da975b564f8074b3cd2e3ac9700e396771
mohaddeseAmel <mohaddese.computer@gmail.com>
2024-05-23 13:08:43 +0430

    fixed header TurnRating

--- src/components/HeaderTurnRating.vue
+++ src/components/HeaderTurnRating.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="Ratings p-5 flex justify-between items-center bg-[#62c17d]">
+      <button class="next flex text-sm  disabled:text-white" disabled> 
+          <svg
+            class="h-5 w-5 text-white"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <polyline points="9 18 15 12 9 6" />
+          </svg>
+          ماه قبل
+        </button>
+         <div class="title">
+          <h1 class="font-bold text-lg text-white">اردیبهشت 1403</h1>
+        </div>
         <button class="prev flex text-sm text-white">
+          ماه بعد
           <svg
             class="h-5 w-5 text-white"
             width="24"
@@ -15,26 +33,7 @@
             <path stroke="none" d="M0 0h24v24H0z" />
             <polyline points="15 6 9 12 15 18" />
           </svg>
-          ماه بعد
         </button>
-        <div class="title">
-          <h1 class="font-bold text-lg text-white">اردیبهشت 1403</h1>
-        </div>
-        <button class="next flex text-sm  disabled:text-white" disabled>
-          ماه قبل
-          <svg
-            class="h-5 w-5 text-white"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <polyline points="9 18 15 12 9 6" />
-          </svg>
-        </button>
-        
       </div>
 </template>
 
