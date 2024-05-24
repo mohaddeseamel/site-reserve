c7500c92c9dae94817bd5faf00174a8eaf3f690e
mohaddeseAmel <mohaddese.computer@gmail.com>
2024-05-25 00:03:46 +0430

    create component question user

---
+++ src/components/questionUser.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="question-user container mx-auto flex flex-col">
+    <div class="title mt-5">
+      <h1 class="lg:text-[40px] text-[30px] text-center text-green-dark">
+        سوالات متداول
+      </h1>
+    </div>
+    
+  </div>
+</template>
+
+<script setup></script>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,10 +2,12 @@
   <div class="overflow-x-hidden">
     <HeaderDashboard />
     <servicesSystem />
+    <questionUser/>
   </div>
 </template>
 
 <script setup>
 import HeaderDashboard from "../components/HeaderDashboard.vue";
 import servicesSystem from "../components/servicesSystem.vue";
+import questionUser from "../components/questionUser.vue"
 </script>
