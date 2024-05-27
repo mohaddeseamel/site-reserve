657db81668d65410dc38e3497be20ad6a78cd109
amel <mohaddese.computer@gmail.com>
2024-05-27 14:29:49 +0330

    change answer accordion and add new component

---
+++ src/components/DescriptionSite.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="contant-site">
+        <div class="lg:text-[40px] text-[30px] text-center text-green-dark mt-6">
+            <h1>توضیحات سامانه</h1>
+        </div>
+    </div>
+</template>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -3,11 +3,13 @@
     <HeaderDashboard />
     <servicesSystem />
     <questionUser/>
+    <DescriptionSite/>
   </div>
 </template>
 
 <script setup>
 import HeaderDashboard from "../components/HeaderDashboard.vue";
 import servicesSystem from "../components/servicesSystem.vue";
-import questionUser from "../components/questionUser.vue"
+import questionUser from "../components/questionUser.vue";
+import DescriptionSite from "../components/DescriptionSite.vue";
 </script>
