888b43f7cccd97d50ac4c77cf589c722b05d8cd9
mohaddeseAmel <mohaddese.computer@gmail.com>
2024-06-03 13:36:45 +0430

    add footer site

---
+++ src/components/FooterSite.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="footer-content py-16 bg-green-light container mx-auto px-10">
+    <div class="header-footer flex justify-between">
+      <div class="logoSite mr-6 flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          lg:width="3em"
+          width="4em"
+          lg:height="3em"
+          height="3em"
+          viewBox="0 0 50 50"
+        >
+          <path
+            fill="#62c17d"
+            d="M34.781 6.664H27.83c-1.256.004-1.209 1.876 0 1.865h6.951v.957H27.83c-1.249-.011-1.203 1.86 0 1.865h6.951v.978H27.83c-1.249 0-1.203 1.87 0 1.865h6.951v.932H27.83c-1.223.009-1.203 1.881 0 1.887h6.951v.934H27.83c-1.223-.004-1.228 1.865 0 1.865h6.951v.932H27.83c-1.223.004-1.228 1.875 0 1.866h6.951v.955H27.83c-1.223-.011-1.228 1.858 0 1.865h6.951v.93H27.83c-1.223-.001-1.203 1.872 0 1.866h6.951v18.159c.004 2.767 4.207 2.717 4.219 0V3.89c-.012-1.264-1.05-2.862-2.758-2.867H27.83c-1.256.001-1.209 1.873 0 1.865l6.951.023v.93H27.83c-1.254-.006-1.207 1.865 0 1.866h6.951zm-7.429 32.194C27.348 35.628 24 32.154 20 34.404V22.05L17.413 1.675c-.054-.539-.511-.666-.818-.675c-.317.009-.952.136-1.021.675L13 22.05v12.354c-4-2.25-7.24 1.246-7.241 4.429c.001 2.832 2.181 5.158 5.131 5.151c2.972.007 5.11-2.6 5.11-5.151V26h1v12.833c0 2.856 2.212 4.97 4.67 5.104c-.041 1.566.47 3.8 1.432 4.686c1.128 1.04 2.471-.29 1.92-1.373c-.532-1.008-1.054-1.605-.63-3.806c1.772-.82 2.956-2.546 2.96-4.586m-16.348 2.541a2.542 2.542 0 0 1 0-5.085c1.386.008 2.515 1.145 2.525 2.544a2.552 2.552 0 0 1-2.525 2.541m8.571-2.541a2.553 2.553 0 0 1 2.526-2.544a2.548 2.548 0 0 1 2.525 2.544a2.545 2.545 0 0 1-2.525 2.541a2.549 2.549 0 0 1-2.526-2.541"
+          />
+        </svg>
+        <span class="text-green-dark lg:text-xl text-lg font-[500]"
+          >Colorful Beauty</span
+        >
+      </div>
+      <div class="btnGoback text-center hover:bg-green-dark flex items-center justify-center bg-white-default w-10 h-10 rounded-full cursor-pointer">
+        <Icon
+          icon="pepicons-pencil:arrow-up"
+          class="text-green-dark hover:text-white-default md:text-5xl text-2xl text-center"
+        />
+      </div>
+    </div>
+    <div
+      class="special-services lg:mt-28 mt-12 flex md:flex-row flex-col items-center justify-around text-center"
+    >
+      <div
+        class="education text-center  flex flex-col items-center justify-center cursor-pointer"
+      >
+        <Icon
+          icon="fluent-mdl2:education"
+          class="text-green-dark md:text-[5em] text-[6em] text-center"
+        />
+        <h1 class="mt-5 md:text-base text-sm dana-medium">دوره های آموزشی</h1>
+      </div>
+
+      <div
+        class="best-beauty text-center mt-6 flex flex-col items-center justify-center cursor-pointer"
+      >
+        <Icon
+          icon="icon-park-outline:beauty"
+          class="text-green-dark md:text-[5em] text-[6em]"
+        />
+        <h1 class="md:text-base text-sm mt-6 dana-medium">
+          بهترین آرایشگران از نظر کاربران
+        </h1>
+      </div>
+      <div
+        class="always-discount mt-5 flex flex-col items-center justify-center cursor-pointer"
+      >
+        <Icon
+          icon="iconamoon:discount"
+          class="text-green-dark md:text-[5em] text-[6em]"
+        />
+        <h1 class="mt-5 md:text-base text-sm dana-medium">همواره تخفیف</h1>
+      </div>
+      <div
+        class="special-discount mt-6 flex flex-col items-center justify-center cursor-pointer"
+      >
+        <Icon
+          icon="tabler:basket-discount"
+          class="text-green-dark md:text-[5em] text-[6em]"
+        />
+        <h1 class="mt-5 md:text-base text-sm dana-medium">
+          تخفیفات لحظه ای ویژه
+        </h1>
+      </div>
+    </div>
+    <div
+      class="contaont-relationship mt-20 mr-6 flex lg:flex-row flex-col items-center justify-between"
+    >
+      <div class="content lg:w-1/2 w-full">
+        <h1 class="dana-bold text-[1.1em] lg:text-right text-center mb-5">رسالت کالرفول بیوتی</h1>
+        <p class="mt-4 text-justify text-lg">
+          هدف کالرفول بیوتی این است که کاربران در کمترین زمان بدون معطل شدن در
+          آرایشگاه ها بتوانند
+          خدمات زیبایی و آرایشی را رزرو کنند، اطلاع رسانی ها و تخفیفات را در
+          کمترین زمان دریافت نمایند.
+        </p>
+      </div>
+
+      <div class="relation-icons ml-6">
+        <div class="icons flex lg:mt-4 mt-10">
+          <Icon icon="skill-icons:instagram" class="ml-5 text-2xl cursor-pointer"/>
+          <Icon icon="logos:telegram" class="ml-5 text-2xl cursor-pointer"/>
+          <Icon icon="devicon:linkedin" class="ml-5 text-2xl cursor-pointer"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<style></style>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -4,6 +4,7 @@
     <servicesSystem />
     <questionUser/>
     <DescriptionSite/>
+    <FooterSite/>
   </div>
 </template>
 
@@ -12,4 +13,5 @@ import HeaderDashboard from "../components/HeaderDashboard.vue";
 import servicesSystem from "../components/servicesSystem.vue";
 import questionUser from "../components/questionUser.vue";
 import DescriptionSite from "../components/DescriptionSite.vue";
+import FooterSite from "../components/FooterSite.vue"
 </script>
